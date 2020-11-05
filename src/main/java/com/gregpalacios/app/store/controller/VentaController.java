a54f94699d44a90ddf68bd4b74222f8cf5b96f19
Gregory Palacios <gregpalacios26@gmail.com>
2020-11-05 18:19:19 -0500

    Agregando CORS

--- src/main/java/com/gregpalacios/app/store/controller/VentaController.java
+++ src/main/java/com/gregpalacios/app/store/controller/VentaController.java
@@ -1,10 +1,13 @@
 package com.gregpalacios.app.store.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,12 @@ public class VentaController {
 
 	@Autowired
 	private IVentaService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Venta>> listar() throws Exception {
+		List<Venta> lista = service.listar();
+		return new ResponseEntity<List<Venta>>(lista, HttpStatus.OK);
+	}
 
 	@PostMapping
 	public ResponseEntity<Venta> registrar(@Valid @RequestBody VentaDetalleVentaDTO dto) throws Exception {
